1a8447b27bc79a1abc11dd602c5d57aa16f8c4df
nkalik <nkalikparedes@frba.utn.edu.ar>
2026-09-25 22:36:15 -0300

    cree la funcion pedirarchivo y empece con la lectura del archivosemanal

--- resumen.cpp
+++ resumen.cpp
@@ -23,3 +23,79 @@ struct resumensemanal{
     int unidadesvendidas;
     float totalcomision;
 };
+
+bool pedirarchivo(char nombrearchivo[]){
+    int semana, mes;
+    
+    cout << "Ingrese la semana (ej 1): ";
+    cin >> semana;
+
+    cout <<"Ingrese el mes (1-12): ";
+    cin >> mes;
+
+    sprintf(nombrearchivo, "comandas_semana_s%d-%02d.dat", semana, mes);
+
+    FILE *archivosemanal = fopen(nombrearchivo, "rb");
+    if(archivosemanal == NULL){
+        cout << "ERROR: No se encuentra " << nombrearchivo << endl;
+        return false;
+    }
+
+    fclose(archivosemanal);
+    return true;
+}
+
+int main(){
+    cout << "          RESUMEN DE CIERRE          " << endl;
+
+    mozo mozos[100];
+    int lenmozos = 0;
+
+    resumensemanal resumen[100];
+    int lenresumen = 0;
+
+    int totalvendido = 0;
+
+    FILE *archivomozos = fopen("mozos.dat", "rb");
+    if(archivomozos == NULL){
+        cout << "ERROR: No se encuentra mozos.dat" << endl;
+        return 1;
+    }
+
+    // Se cargan los mozos en memoria
+    while(fread(&mozos[lenmozos], sizeof(mozo), 1, archivomozos) == 1){
+        lenmozos++;
+    }
+
+    fclose(archivomozos);
+
+    char nombrearchivo[50];
+
+    // Llama a la funcion y en caso que devuelva false la ejecuta hasta devolver true
+    while(!pedirarchivo(nombrearchivo)){
+        cout << "Intente otra vez.\n" << endl;
+    }
+
+    FILE *archivosemanal = fopen(nombrearchivo, "rb");
+    if(archivosemanal == NULL){
+        cout <<"Error al abrir " << nombrearchivo << endl;
+        return 1;
+    }
+
+    comanda aux;
+
+    while(fread(&aux, sizeof(comanda), 1, archivosemanal) == 1){
+
+        resumen[lenresumen].idmozo = aux.idmozo;
+        resumen[lenresumen].lenventas++;
+        resumen[lenresumen].unidadesvendidas += aux.cantidad;
+        resumen[lenresumen].totalcomision += aux.comision;
+
+        lenresumen++;
+
+        totalvendido += aux.cantidad;
+    }
+
+    fclose(archivosemanal);
+    return 0;
+}
